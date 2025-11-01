973a2a2877c9e5b39328952ecd3c1eb6c4b634b9
Davies Wabuluka <dwabuluka@gmail.com>
2025-10-31 21:13:34 -0500

    added cron jobs

--- src/server.js
+++ src/server.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import { initDB, sql } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRouter from "./routes/transactions.js";
+import job from "./config/cron.js";
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start;
+
 app.use(rateLimiter);
 app.use(express.json());
+
+app.get("/api/health", (req, res) => [res.status(200).json({ status: "Ok" })]);
 
 app.use("/api/transactions", transactionsRouter);
 
